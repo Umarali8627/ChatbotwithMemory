9f94fe9b99ca3a3e984d9aaba463415ec30b26cc
Umar Ali <your_github_email@example.com>
2026-02-21 22:55:33 +0500

    adding unique id for session

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import streamlit as st
 from backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
-CONFIG= {'configurable':{'thread_id':'thread-1'}}
 st.set_page_config(page_title="LangGraph ChatBot",page_icon=":mortar_board",layout='centered')
 
 st.title("U-Chat")
 st.caption("U-Chat Remember everyting")
 if "messages" not in st.session_state:
     st.session_state.messages = []
+if "thread_id" not in st.session_state:
+    st.session_state.thread_id = str(uuid.uuid4())
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -24,7 +26,8 @@ if user_question:
        with st.spinner('Thinking...'):
         ai_message=''
         try:
-           response= chatbot.invoke({'messages':[HumanMessage(content=user_question)]},config=CONFIG)
+           config = {"configurable": {"thread_id": st.session_state.thread_id}}
+           response= chatbot.invoke({'messages':[HumanMessage(content=user_question)]},config=config)
            ai_message=response['messages'][-1].content
         except Exception as ex:
             st.markdown(ex)

dc54f8bc8c6eccb8caa89415acea4d7a2e33b392
Umar Ali <your_github_email@example.com>
2026-02-21 22:57:42 +0500

    revert the unique id code

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import streamlit as st
 from backend import chatbot
 from langchain_core.messages import HumanMessage
-import uuid
 
+CONFIG= {'configurable':{'thread_id':'thread-1'}}
 st.set_page_config(page_title="LangGraph ChatBot",page_icon=":mortar_board",layout='centered')
 
 st.title("U-Chat")
 st.caption("U-Chat Remember everyting")
 if "messages" not in st.session_state:
     st.session_state.messages = []
-if "thread_id" not in st.session_state:
-    st.session_state.thread_id = str(uuid.uuid4())
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -26,8 +24,7 @@ if user_question:
        with st.spinner('Thinking...'):
         ai_message=''
         try:
-           config = {"configurable": {"thread_id": st.session_state.thread_id}}
-           response= chatbot.invoke({'messages':[HumanMessage(content=user_question)]},config=config)
+           response= chatbot.invoke({'messages':[HumanMessage(content=user_question)]},config=CONFIG)
            ai_message=response['messages'][-1].content
         except Exception as ex:
             st.markdown(ex)
